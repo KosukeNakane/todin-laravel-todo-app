76585578509711d6cfc86f3326ca47d0518efef0
KosukeNakane <kd1429179@st.kobedenshi.ac.jp>
2025-11-09 20:47:55 +0900

    🎉 First commit

---
+++ database/migrations/2025_11_07_021340_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->date('due_date')->nullable();
+            $table->tinyInteger('priority')->default(0);
+            $table->boolean('is_completed')->default(false);
+            $table->text('description')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps(); // created_at, updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
